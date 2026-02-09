796121f0e343fdeba1ec02f09795f54d4bf0a7aa
Büşra Hızlıol <busrahizliol@gmail.com>
2026-02-09 04:00:22 +0300

    Fix CORS for GitHub Pages

--- backend.py
+++ backend.py
@@ -18,11 +18,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
-    allow_credentials=True,
+    allow_origins=["*"],   # TEST için
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
 
